31db427288e1a9f4d31d54188e90b994957bffe3
huttongrabiel <huttonthomas@icloud.com>
2022-07-18 11:34:27 -0700

    WhosLive: Find out if your favorite twitch stream is live
    Just pass -u <username> to whos_live and find out immediately if your
    favorite twitch streamer is live. If they are, great, you get a nice
    message as well as a link to their stream. Otherwise, you get a sad
    face.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,99 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use tokio;
+
+struct TwitchRequest {
+    url: String,
+}
+
+impl TwitchRequest {
+    fn new(username: &String) -> Self {
+        Self {
+            url: format!("https://www.twitch.tv/{}", username),
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    username: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let request = TwitchRequest::new(&args.username);
+    let response = make_request(&request).await;
+
+    if is_live(response) {
+        diplay_live_user(args.username, request.url)
+    } else {
+        println!("{} is not live. :(", args.username)
+    }
+}
+
+/// Query the url of a twitch stream at "twitch.tv/{username}".
+///
+/// Returns html contents of page when can be searched to determine
+/// whether a streamer is live or not.
+///
+/// # Panics
+///
+/// Potential for await to return an error.
+async fn make_request(request_content: &TwitchRequest) -> String {
+    let response = reqwest::get(&request_content.url)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    response
+}
+
+/// Looks through the response returned by make_request for "isLiveBroadcast".
+///
+/// isLiveBroadcast only appears in the response when the streamer is live.
+fn is_live(response: String) -> bool {
+    // No Twitch API in this town! Only live channles have isLiveBroadcast in
+    // the inspect source. 100000 IQ
+    if response.contains("isLiveBroadcast") {
+        return true;
+    }
+    false
+}
+
+/// Prints a box with "<username> is live!" and the link to the stream in it.
+///
+/// # Panics
+///
+/// Slicing the string could panic if the character is not ascii.
+fn diplay_live_user(username: String, url: String) {
+    // + 4 accounts for the border of the square and the space on each side.
+    let x = url.len() + 4;
+    let y = 6;
+
+    let live_message = format!("{} is live!", username);
+    let live_message_start_index = (x - live_message.len()) / 2;
+
+    for i in 0..y {
+        for j in 0..x {
+            if i == 0 || i == y - 1 || j == 0 || j == x - 1 {
+                print!("*");
+            } else if i == 2
+                && (j > live_message_start_index
+                    && j < live_message.len() + live_message_start_index)
+            {
+                let index = j - live_message_start_index - 1;
+                print!("{}", live_message.get(index..index + 1).unwrap());
+            } else if i == 3 && (j > 1 && j < x - 2) {
+                let index = j - 2;
+                print!("{}", url.get(index..index + 1).unwrap());
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
 }
